66b63b748036913949c55be12fba1635ab9f3774
igor <ehotsureev@gmail.com>
2024-06-16 21:05:00 +0300

    add ChatPT api

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,4 +1,27 @@
+from openai import OpenAI
+from dotenv import load_dotenv
 
+load_dotenv()
 
 if __name__ == '__main__':
-    pass
+    flag = True
+    while flag:
+        massage = input()
+        if massage == "stop":
+            flag = False
+            break
+
+        client = OpenAI(
+                        base_url="https://api.proxyapi.ru/openai/v1")
+
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+                {"role": "user", "content": f"{massage}"}
+            ],
+            max_tokens=50
+        )
+
+        print(completion.choices[0].message.content)
